5bac894b9cbbf383f9689d88d39939dc7000a03c
Fernando Maclen <fmaclen@gmail.com>
2014-11-16 14:00:02 -0800

    Agregado de Favicons + Mejoras de estilos

--- public/js/main.js
+++ public/js/main.js
@@ -22,3 +22,39 @@ $(function() {
   $('a[name="' + window.location.hash.replace(/#/, '') + '"]').next('section').addClass('resaltado')
 });
 
+// Animacion al cambiar de articulo
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+// Animacion del Header
+
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 500,
+            header = document.querySelector("#principal");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"achicado");
+        } else {
+            if (classie.has(header,"achicado")) {
+                classie.remove(header,"achicado");
+            }
+        }
+    });
+}
+window.onload = init();
+
